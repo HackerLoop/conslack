2ac56e440097d7d65c1fbc0d00a851f7b9717d9b
jhchabran <jh@chabran.fr>
2016-11-14 20:30:04 +0100

    Add a proper ^T
    This is still very rough but it's working.

--- ui/ui.go
+++ ui/ui.go
@@ -81,13 +81,20 @@ func (a *App) Loop() error {
 func (a *App) prepare() error {
 	a.g.Highlight = true
 	a.g.Cursor = true
-	a.g.SelFgColor = gocui.ColorGreen
+	a.g.SelFgColor = gocui.ColorBlack
+	a.g.SelBgColor = gocui.ColorWhite
+	a.g.Mouse = false
+	a.g.InputEsc = true
 
 	a.maxX, a.maxY = a.g.Size()
 
 	a.g.SetManagerFunc(a.layout)
 
 	if err := a.g.SetKeybinding("input", gocui.KeyEnter, gocui.ModNone, a.sendMessage); err != nil {
+		return err
+	}
+
+	if err := a.g.SetKeybinding("", gocui.KeyCtrlT, gocui.ModNone, a.jumpToTopic); err != nil {
 		return err
 	}
 
@@ -113,11 +120,191 @@ func (a *App) sendMessage(g *gocui.Gui, v *gocui.View) error {
 	return nil
 }
 
+func (a *App) jumpToTopic(g *gocui.Gui, v *gocui.View) error {
+	ticker := time.NewTicker(100 * time.Millisecond)
+
+	closeFn := func(g *gocui.Gui, _v *gocui.View) error {
+		ticker.Stop()
+
+		if _, err := g.SetViewOnTop("messages"); err != nil {
+			return err
+		}
+
+		if _, err := g.SetViewOnTop("status"); err != nil {
+			return err
+		}
+
+		if _, err := g.SetViewOnTop("input"); err != nil {
+			return err
+		}
+
+		if _, err := g.SetCurrentView("input"); err != nil {
+			return err
+		}
+
+		return nil
+	}
+
+	// TODO https://github.com/nsf/termbox-go/issues/120
+	if err := g.SetKeybinding("jump.container", gocui.KeyEsc, gocui.ModNone, closeFn); err != nil {
+		logrus.Error("can't assign key")
+		return err
+	}
+	if err := g.SetKeybinding("jump.input", gocui.KeyEsc, gocui.ModNone, closeFn); err != nil {
+		logrus.Error("can't assign key")
+		return err
+	}
+
+	// if err := g.SetKeybinding("jump.input", gocui.KeyEnter, gocui.ModNone, jumpToSelected()); err != nil {
+	// 	logrus.Error("can't assign key")
+	// 	return err
+	// }
+
+	c, err := g.SetViewOnTop("jump.container")
+	if err != nil {
+		return err
+	}
+	c.Clear()
+
+	i, err := g.SetViewOnTop("jump.input")
+	if err != nil {
+		return err
+	}
+	i.Clear()
+	i.SetCursor(0, 0)
+
+	// TODO rename to topics
+	discussions, err := a.c.GetDiscussions()
+	logrus.Infof("%#v", discussions)
+	if err != nil {
+		return err
+	}
+
+	nextTopicFn := func(g *gocui.Gui, v *gocui.View) error {
+		logrus.Info("next")
+		g.Execute(func(g *gocui.Gui) error {
+			c.MoveCursor(0, 1, false)
+			return nil
+		})
+
+		return nil
+	}
+	prevTopicFn := func(g *gocui.Gui, v *gocui.View) error {
+		logrus.Info("prev")
+		g.Execute(func(g *gocui.Gui) error {
+			c.MoveCursor(0, -1, false)
+			return nil
+		})
+
+		return nil
+	}
+
+	enterFn := func(g *gocui.Gui, v *gocui.View) error {
+		logrus.Info("prev")
+
+		_, y := c.Cursor()
+		topic, err := c.Line(y)
+		if err != nil {
+			return err
+		}
+
+		err = closeFn(g, nil)
+		if err != nil {
+			return err
+		}
+
+		topic = strings.TrimSpace(topic)
+		if topic == "" {
+			return nil
+		}
+
+		go func() {
+			if err := a.openTopic(g, topic); err != nil {
+				logrus.WithError(err).Error("can't open topic")
+				return
+			}
+		}()
+
+		return nil
+	}
+
+	if err := g.SetKeybinding("jump.input", gocui.KeyArrowDown, gocui.ModNone, nextTopicFn); err != nil {
+		logrus.Error("can't assign key")
+		return err
+	}
+	if err := g.SetKeybinding("jump.input", gocui.KeyArrowUp, gocui.ModNone, prevTopicFn); err != nil {
+		logrus.Error("can't assign key")
+		return err
+	}
+	if err := g.SetKeybinding("jump.input", gocui.KeyEnter, gocui.ModNone, enterFn); err != nil {
+		logrus.Error("can't assign key")
+		return err
+	}
+
+	if err := c.SetCursor(0, 1); err != nil {
+		return err
+	}
+
+	g.Execute(func(g *gocui.Gui) error {
+		fmt.Fprintf(c, "\n\n") // skip input view
+
+		for _, d := range discussions {
+			fmt.Fprintf(c, "%s\n", d.Name)
+		}
+
+		return nil
+	})
+
+	if _, err := g.SetCurrentView("jump.input"); err != nil {
+		return err
+	}
+
+	go func() {
+		oldQuery := ""
+
+		for _ = range ticker.C {
+			query := strings.TrimSpace(i.ViewBuffer())
+			if query == oldQuery {
+				continue
+			}
+
+			matches := []conslack.Discussion{}
+			for _, d := range discussions {
+				if strings.HasPrefix(d.Name, query) || strings.HasPrefix(d.Name, "#"+query) {
+					matches = append(matches, d)
+				}
+			}
+
+			g.Execute(func(g *gocui.Gui) error {
+				c.Clear()
+
+				fmt.Fprintf(c, "\n\n") // TODO an abstraction is fucking required for dealing with rendering
+
+				for _, d := range matches {
+					fmt.Fprintf(c, "%s\n", d.Name)
+				}
+
+				return nil
+			})
+
+			oldQuery = query
+		}
+	}()
+
+	return nil
+}
+
 func formatMessage(m *conslack.Message) string {
 	return fmt.Sprintf("%s %s %s\n", m.Date, m.From, m.Text)
 }
 
 func (a *App) openTopic(g *gocui.Gui, topicName string) error {
+	logrus.WithField("topic", topicName).Info("opening topic")
+	if a.currentTopic.close != nil {
+		logrus.Info("close current topic")
+		a.currentTopic.close()
+	}
+
 	v, err := g.View("messages")
 	if err != nil {
 		logrus.WithError(err).Error("unable to get messages view")
@@ -135,6 +322,7 @@ func (a *App) openTopic(g *gocui.Gui, topicName string) error {
 	}
 
 	g.Execute(func(g *gocui.Gui) error {
+		h.Clear()
 		fmt.Fprintf(h, topicName)
 		return nil
 	})
@@ -187,6 +375,7 @@ func (a *App) layout(g *gocui.Gui) error {
 		v.Wrap = false
 		v.Frame = true
 		v.Overwrite = true
+		v.Highlight = true
 
 		g.Execute(func(g *gocui.Gui) error {
 			v.FgColor = gocui.ColorWhite
